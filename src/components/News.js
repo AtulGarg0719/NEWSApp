1909fd9aa85f91d56a97ee5ea7d52c2e05c7123a
Atul Agrawal <atul60556@gmail.com>
2022-07-30 23:03:00 +0530

    First Commit

---
+++ src/components/News.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+    /*articles = [
+        {
+            "source": {
+                "id": "bbc-sport",
+                "name": "BBC Sport"
+            },
+            "author": "BBC Sport",
+            "title": "Shane Warne memorial - watch & follow updates",
+            "description": "Watch live coverage and follow text updates and tributes from the state memorial for Australian cricket legend Shane Warne at the Melbourne Cricket Ground.",
+            "url": "http://www.bbc.co.uk/sport/live/cricket/60916236",
+            "urlToImage": "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png",
+            "publishedAt": "2022-03-30T08:22:26.498888Z",
+            "content": "Former England bowler and BBC cricket presenter Isa Guha, who became a colleague of Warne's in the commentary box: \"It has been a strange few weeks - a lot of shock and then we did our own tribute at… [+396 chars]"
+        },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+            "publishedAt": "2020-04-27T11:41:47Z",
+            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+        },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+            "publishedAt": "2020-03-30T15:26:05Z",
+            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+        }
+    ];*/
+    constructor(){
+        super();
+        console.log("news contructor");
+        this.state = {
+            article : [],
+            page:1,
+        }
+    }
+    async componentDidMount(){
+        let url='https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=b51ce35cc45749c38e6b0a768f695486&page=1&pageSize=15';
+        let data = await fetch(url);
+        let jsondata = await data.json();
+        console.log(jsondata);
+        this.setState({
+            article:jsondata.articles,
+            totalArticles:jsondata.totalResults,
+        })
+    }
+    handelPervNews = async()=>{
+        console.log('Previous call');
+        let url=`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=b51ce35cc45749c38e6b0a768f695486&page=${this.state.page-1}&pageSize=15`;
+        let data = await fetch(url);
+        let jsondata = await data.json();
+        console.log(jsondata);
+        this.setState({
+            page:this.state.page-1,
+            article:jsondata.articles,
+        })
+    }
+    handelNextNews = async()=>{
+        console.log("Next Call");   
+        if(this.state.page+1 > Math.ceil(this.state.totalArticles/15)){
+
+        }else{
+        let url=`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=b51ce35cc45749c38e6b0a768f695486&page=${this.state.page+1}&pageSize=15`;
+        let data = await fetch(url);
+        let jsondata = await data.json();
+        console.log(jsondata);
+        this.setState({
+            page:this.state.page+1,
+            article:jsondata.articles,
+        })
+    }
+    }
+  render() {
+    return (
+      <div className='container my-3'>
+            <h1>News App - Top HeadLines</h1>
+            <div className='row mx-auto'>
+                {this.state.article.map((element)=>{
+                    return <div className='col-md-4 my-2' key={element.url}> 
+                    <NewsItem  title={element.title} description={element.description} imgurl={element.urlToImage} newsUrl={element.url}/>
+                </div>
+                })}
+            </div>
+        <div className='div d-flex justify-content-between my-2'>
+        <button type="button" onClick={this.handelPervNews} disabled={this.state.page<=1} className="btn btn-dark">&larr; Previous</button>
+        <button type="button" onClick={this.handelNextNews} className="btn btn-dark">Next &rarr;</button>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+export default News
